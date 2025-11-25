56ae22502a092442b6b06dff6c6dbb4bcb593f45
Javier Bueno Calzadilla <144735820+Javier-Bueno@users.noreply.github.com>
2025-11-25 13:44:51 +0000

    Add files via upload

---
+++ urban_pigeon_hunter.py
@@ -0,0 +1,12 @@
+# Universidad de La Laguna
+# Grado en Ingeniería Informática
+# Sistemas de Interacción Persona-Computador
+# Práctica 3: Interfaces gestuales
+# Componentes del grupo:
+# - Javier Bueno Calzadilla - alu0101627922
+# - Adriel Reyes Suárez
+# - Carlos Pérez Gómez
+# Versión: 1.0.0
+# Historial de modificaciones con versiones:
+# Versión 1.0.0
+# - Creación inicial del código
