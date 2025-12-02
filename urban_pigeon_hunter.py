bbbab5d61a5d62cad736562cbb2db6e554da7610
Javier Bueno Calzadilla <144735820+Javier-Bueno@users.noreply.github.com>
2025-12-02 14:20:15 +0000

    Version 0.0.2

--- urban_pigeon_hunter.py
+++ urban_pigeon_hunter.py
@@ -8,8 +8,11 @@
 # - Carlos Pérez Gómez
 # Versión: 1.0.0
 # Historial de modificaciones con versiones:
-# Versión 1.0.0
+# Versión 0.0.1
 # - Creación inicial del código
+# Versión 0.0.2
+# - Modificaciones: Aparición de cazador, proyectil, proyectil de la paloma
+
 
 import sys, random
 random.seed(1) # Hace que la simulación sea igual cada vez, más fácil de depurar
@@ -18,28 +21,51 @@ import pymunk
 import pymunk.pygame_util
 
 # Tamaño de la pantalla
-display_h = 600
-display_w = 600
+display_h = 800
+display_w = 800
+
+# Configuraciones de la simulación
+FPS = 50
+TICKS_MIN = 1 * FPS  # 50 ticks (1 segundo)
+TICKS_MAX = 2 * FPS  # 100 ticks (2 segundos)
+
+# Configuraciones de las imágenes
+scale_width = 29 * 1.5
+scale_height = 29 * 1.5
+
+# Animaciones de las palomas:
+FRAME_INTERVAL = FPS * 1.5 # Las palomas cambian de imagen cada 1.5 segundos
 
 # Fucniones para añadir los objetos al espacio de pymunk
-def add_pidgeon(space):
+def add_lr_pidgeon(space): # Paloma que aparece por la izquierda y desaparece por la derecha
     mass = 3
-    radius = 20
+    # radius = 20
+    width = scale_width
+    height = scale_height
     body = pymunk.Body(body_type=pymunk.Body.KINEMATIC) # Creamos el cuerpo de la paloma cinemático
-    x = random.choice([0, 600])  # Seleccionamos el rango en x en que se va a crear (bordes de la pantalla)
-    y = random.randint(120, 480)  # Seleccionamos el rango en y en que se va a crear
+    x = random.choice([0, display_w])  # Seleccionamos el rango en x en que se va a crear (bordes de la pantalla)
+    y = random.randint(375, display_h - 50)  # Seleccionamos el rango en y en que se va a crear
     body.position = x, y # Fijamos su posición
     if x == 0:
-        body.velocity = 100, 0 # Asignamos una velocidad aleatoria en x
+        body.velocity = 120, 0 # Asignamos una velocidad aleatoria en x
+        direction = "RIGHT" # La paloma surge del origen (para diferenciar sprites)
     else:
-        body.velocity = -100, 0 # Asignamos una velocidad aleatoria en x
-    
-    shape = pymunk.Circle(body, radius) # Creamos una forma circular para que el cuerpo pueda colisionar
+        body.velocity = -120, 0 # Asignamos una velocidad aleatoria en x
+        direction = "LEFT" # La paloma surge de la derecha (para diferenciar sprites)
+    shape = pymunk.Poly.create_box(body, (width, height)) # Creamos una forma rectangular para que el cuerpo pueda colisionar
     shape.mass = mass # Asignamos la masa a la forma 
+    shape.width = width
+    shape.height = height
+    shape.direction = direction
+    shape.animation_frame_index = 0 # Índice inicial para el conjunto con las imágenes de la paloma
+    shape.animation_timer  = FRAME_INTERVAL # Temporizador para saber cuando cambiar la imagen
+
     space.add(body, shape) # Añadimos el cuerpo con su forma al espacio
+
     return shape
 
-def add_proyectile(space, position):
+
+def add_proyectile(space, position): # Proyectil de la paloma
     mass = 1
     radius = 5
     body = pymunk.Body(body_type=pymunk.Body.DYNAMIC) # Creamos el cuerpo del proyectil dinámico
@@ -49,7 +75,7 @@ def add_proyectile(space, position):
     space.add(body, shape)
     return shape
 
-def add_hunter(space):
+def add_hunter(space): # Cazador de palomas
     mass = 10
     width = 20
     height = 60
@@ -75,11 +101,18 @@ def add_gun(space, hunter):
 # Funciones para dibujar los objetos en pantalla (HAY QUE REVISARLAS)
 def draw_pidgeon(screen, pidgeon):
     p = int(pidgeon.body.position.x), display_h - int(pidgeon.body.position.y)
-    pygame.draw.circle(screen, (0,0,255), p, int(pidgeon.radius), 2)
+    #pygame.draw.circle(screen, (0,0,255), p, int(pidgeon.radius), 2)
+    pygame.draw.rect(screen, (255,0,0), (p[0] - pidgeon.width / 2, p[1] - pidgeon.height / 2, pidgeon.width, pidgeon.height), 2)
 
-def draw_pidgeon_with_image(screen,pidgeon,image):
-    p = int(pidgeon.body.position.x - pidgeon.radius), (display_h - int(pidgeon.body.position.y))- pidgeon.radius
-    screen.blit(image, p)
+def draw_pidgeon_with_image(screen,pidgeon,lr_frame, rl_frame):
+    current_index = pidgeon.animation_frame_index
+    if pidgeon.direction == "LEFT":
+        image_to_draw = rl_frame[current_index]
+    else:
+        image_to_draw = lr_frame[current_index]
+        
+    p = int(pidgeon.body.position.x - pidgeon.width / 2), (display_h - int(pidgeon.body.position.y + (pidgeon.height / 2)))
+    screen.blit(image_to_draw, p)
 
 def draw_proyectile(screen, proyectile):
     p = int(proyectile.body.position.x), display_h - int(proyectile.body.position.y)
@@ -105,6 +138,15 @@ def draw_gun_with_image(screen,gun,image):
     p = int(gun.body.position.x - gun.radius), (display_h - int(gun.body.position.y))- gun.radius
     screen.blit(image, p)
 
+def update_pidgeon_animation(pidgeon):
+    pidgeon.animation_timer -= 1
+    if pidgeon.animation_timer <= 0:
+        # reiniciamos el temporizador
+        pidgeon.animation_timer = FRAME_INTERVAL
+
+        # cambiamos a la siguiente imagen
+        pidgeon.animation_frame_index = (pidgeon.animation_frame_index + 1) % 2 # que no supere el tamaño del vector de imágenes
+
 # Pasar de pymunk a pygame la posición
 def to_pygame(p):
     """Small helper to convert Pymunk vec2d to Pygame integers"""
@@ -113,10 +155,27 @@ def to_pygame(p):
 
 
 def main():
-    #Cargamos y escalamos las imagenes
-    image_pidgeon= pygame.image.load("basketball.png")
-    image_pidgeon = pygame.transform.scale(image_pidgeon,(20*2,20*2 ))
+
     
+    #Cargamos y escalamos las imagenes
+    image_lr_pidgeon_open = pygame.image.load("ID-PALOMA-2.png")
+    image_lr_pidgeon_open = pygame.transform.scale(image_lr_pidgeon_open,(scale_width,scale_height ))
+
+    image_lr_pidgeon_close = pygame.image.load("ID-PALOMA-1.png")
+    image_lr_pidgeon_close = pygame.transform.scale(image_lr_pidgeon_close,(scale_width,scale_height ))
+
+    # Agrupamos las imágenes que componen el movimiento de la paloma que surge de la izquierda
+    left_pidgeon_frame = [image_lr_pidgeon_open, image_lr_pidgeon_close]
+
+    image_rl_pidgeon_open = pygame.image.load("DI-PALOMA-1.png")
+    image_rl_pidgeon_open = pygame.transform.scale(image_rl_pidgeon_open,(scale_width,scale_height ))
+
+    image_rl_pidgeon_close = pygame.image.load("DI-PALOMA-2.png")
+    image_rl_pidgeon_close = pygame.transform.scale(image_rl_pidgeon_close,(scale_width,scale_height ))
+
+    # Agrupamos las imágenes que componen el movimiento de la paloma que surge de la derecha
+    right_pidgeon_frame = [image_rl_pidgeon_open, image_rl_pidgeon_close]
+
     image_proyectile= pygame.image.load("basketball.png")
     image_proyectile = pygame.transform.scale(image_proyectile,(5*2,5*2 ))
     
@@ -131,7 +190,7 @@ def main():
     # Definimos el tamaño (en píxeles) de la ventana de visualización en PyGame
     screen = pygame.display.set_mode((display_w, display_h))
     # Añadimos un título a la ventana
-    pygame.display.set_caption("Acaba con las palomas urbanas!")
+    pygame.display.set_caption("El destino de la humanidad depende de ti. ¡Acaba con las palomas marroquíes!")
     clock = pygame.time.Clock()
     # Inicializamos el espacio en pymunk
     space = pymunk.Space()
@@ -156,14 +215,14 @@ def main():
  
         ticks_to_next_pidgeon -= 1   # Decrementamos el contador de ticks para la siguiente paloma
         if ticks_to_next_pidgeon <= 0:  # Si ha llegado a cero, añadimos una nueva paloma
-            ticks_to_next_pidgeon = 20   # Reiniciamos el contador de ticks
-            pidgeon_shape = add_pidgeon(space)     # Añadimos la paloma al espacio
+            ticks_to_next_pidgeon = random.randint(TICKS_MIN, TICKS_MAX)  # Reiniciamos el contador de ticks para que tarde de 1 a 2 s en cargar una paloma
+            pidgeon_shape = add_lr_pidgeon(space)     # Añadimos la paloma al espacio
             pidgeons.append(pidgeon_shape)     # Y la añadimos a la lista de palomas
         
         # La función 'step' hace avanzar la simulación un paso (en seg.) en el tiempo cada vez que se la llama.
         # Es mejor usar un paso constante y no ajustarlo en función de lo que tarde cada iteración del bucle. 
         # Debe estar coordinado con lo FPS definidos en PyGame.
-        space.step(1/50.0)
+        space.step(1/FPS)
 
         # Rellenamos la ventana con un fondo blanco
         screen.fill((255,255,255))
@@ -171,9 +230,10 @@ def main():
         pidgeons_to_remove = []  # Lista para almacenar las palomas que deben ser eliminadas
          # Dibujamos las palomas
         for pidgeon in pidgeons:
+            update_pidgeon_animation(pidgeon)
             draw_pidgeon(screen,pidgeon)
-            draw_pidgeon_with_image(screen,pidgeon, image_pidgeon)
-            if pidgeon.body.position.x > 550 or pidgeon.body.position.x < 50:   # Si la paloma sale de la pantalla, la marcamos para eliminarla
+            draw_pidgeon_with_image(screen,pidgeon, left_pidgeon_frame, right_pidgeon_frame)
+            if pidgeon.body.position.x > 850 or pidgeon.body.position.x < -50:   # Si la paloma sale de la pantalla, la marcamos para eliminarla
                 pidgeons_to_remove.append(pidgeon)
         # Eliminamos las palomas que han salido de la pantalla        
         for pidgeon in pidgeons_to_remove:
@@ -186,7 +246,7 @@ def main():
         pygame.display.flip()
 
         # Limita la velocidad del juego a 50 FPS
-        clock.tick(50)
+        clock.tick(FPS)
 
 if __name__ == '__main__':
     sys.exit(main())
